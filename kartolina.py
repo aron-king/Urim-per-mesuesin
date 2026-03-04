3c1ac9e5453b67ebb9d3a293f71923fdf567ab0c
aron-king <aronlici89@gmail.com>
2026-03-04 20:42:51 +0100

    Update kartolina.py

--- kartolina.py
+++ kartolina.py
@@ -3,29 +3,29 @@ st.set_page_config(page_title="Kartolin per 7 mars")
 st.subheder("Nje kartolin per ty")
 urim_per_mesuesit={
     "Xheni":"Ju falenderojm per dijet qe na jepni ne teknologji",
-    "Gjadiolja":"JU falenderoj per dijet qe na jepni ne anglisht",
-    "Andon":"JU falenderoj per dijet qe na jepni ne art",
-    "Luli":"JU falenderoj per dijet qe na jepni ne biologji",
-    "Luiza":"JU falenderoj per dijet qe na jepni ne matematik",
-     "Yllka":"JU falenderoj per dijet qe na jepni ne biologji",
-     "Andi":"JU falenderoj per dijet qe na jepni ne muzik",
-     "Loreta":"JU falenderoj per dijet qe na jepni ne qytetari",
-     "Laura":"JU falenderoj per dijet qe na jepni ne matematik",
-     "Rexhina":"JU falenderoj per dijet qe na jepni ne fizik",
-     "Albana Bega":"JU falenderoj per drejtimin e shkolles ne nje menyr te drejt",
-     "Albana Agalliu":"JU falenderoj per mbeshtetjen dhe keshillat qe na jep",
-     "Aibana":"JU falenderoj per dijet qe na jepni ne frengjisht",
-     "Majlinda":"JU falenderoj per dijet qe na jepni ne gjuhe shqipe",
-     "Luli":"JU falenderoj per dijet qe na jepni ne biologji dhe kimi",
-     "Genta":"JU falenderoj per mbikqyrjen dhe prezencen qe keni ne drejtori",
-     "Arta":"JU falenderoj per kontrollin dhe mbeshtetjen qe na jepni",
-     "Paçi":"JU falenderoj per dijet qe na jepni ne aftesim teknologjik",
-     "Zeni":"JU falenderoj per dijet qe na jepni ne fizkultur"
-     "Egla":"JU falenderoj per dijet qe na jepni ne gjuhe shqipe",
-     "Erdita":"JU falenderoj per dijet qe na jepni ne gjuhe shqipe",
-     "Vojsava":"JU falenderoj per dijet qe na jepni ne histori",
-     "Margarita":"JU falenderoj per dijet qe na jepni ne gjuhe shqipe",
-     "Anila":"JU falenderoj per dijet qe na jepni ne gjografi",
+    "Gjadiolja":"Ju falenderoj per dijet qe na jepni ne anglisht",
+    "Andon":"Ju falenderoj per dijet qe na jepni ne art",
+    "Luli":"Ju falenderoj per dijet qe na jepni ne biologji",
+    "Luiza":"Ju falenderoj per dijet qe na jepni ne matematik",
+     "Yllka":"Ju falenderoj per dijet qe na jepni ne biologji",
+     "Andi":"Ju falenderoj per dijet qe na jepni ne muzik",
+     "Loreta":"Ju falenderoj per dijet qe na jepni ne qytetari",
+     "Laura":"Ju falenderoj per dijet qe na jepni ne matematik",
+     "Rexhina":"Ju falenderoj per dijet qe na jepni ne fizik",
+     "Albana Bega":"Ju falenderoj per drejtimin e shkolles ne nje menyr te drejt",
+     "Albana Agalliu":"Ju falenderoj per mbeshtetjen dhe keshillat qe na jep",
+     "Aibana":"Ju falenderoj per dijet qe na jepni ne frengjisht",
+     "Majlinda":"Ju falenderoj per dijet qe na jepni ne gjuhe shqipe",
+     "Luli":"Ju falenderoj per dijet qe na jepni ne biologji dhe kimi",
+     "Genta":"Ju falenderoj per mbikqyrjen dhe prezencen qe keni ne drejtori",
+     "Arta":"Ju falenderoj per kontrollin dhe mbeshtetjen qe na jepni",
+     "Paçi":"Ju falenderoj per dijet qe na jepni ne aftesim teknologjik",
+     "Zeni":"Ju falenderoj per dijet qe na jepni ne fizkultur"
+     "Egla":"Ju falenderoj per dijet qe na jepni ne gjuhe shqipe",
+     "Erdita":"Ju falenderoj per dijet qe na jepni ne gjuhe shqipe",
+     "Vojsava":"Ju falenderoj per dijet qe na jepni ne histori",
+     "Margarita":"Ju falenderoj per dijet qe na jepni ne gjuhe shqipe",
+     "Anila":"Ju falenderoj per dijet qe na jepni ne gjografi",
 
 }
 emri=st.text_inpup("Vendosni emrin tuaj: ")
@@ -56,6 +56,7 @@ if st.button("🌷Shfaq urimin🌷"):
                 </div>
 
                   """, unsafe_allow_html=True)
+
 
 
 
